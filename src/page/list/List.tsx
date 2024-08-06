212e63668ac74cea15bfd5215515e9c4dcb0cf9f
tipi <tipi@nc-ass-vip.sdv.fr>
2024-08-06 15:52:52 +0200

    add routes

--- src/page/list/List.tsx
+++ src/page/list/List.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
 const List = () => {
+  const employeeList = JSON.parse(localStorage.getItem('employeeList') || '[]')
   return (
-    <div>list</div>
+    <div>
+      <h1>List</h1>
+      <ul>
+      {employeeList.map((employee, index) => (
+        <li key={index}>
+          {employee.firstName} {employee.lastName}
+        </li>
+      ))}
+      </ul>
+    </div>
   )
 }
 
